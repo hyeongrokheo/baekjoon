fbc7f2a8e91a687adca173bb14610bb44e4a900d
hyeongrokheo <hhr@hhr.kr>
2021-07-06 17:32:49 +0900

    210706

---
+++ [9657] 돌 게임 3.py
@@ -0,0 +1,9 @@
+"""
+problem tier : Silver 3 (solved.ac)
+"""
+
+import sys
+sys.stdin = open('./input.txt', 'r')
+input = sys.stdin.readline
+
+
---
+++ [9659] 돌 게임 5.py
@@ -0,0 +1,15 @@
+"""
+problem tier : Silver 2 (solved.ac)
+"""
+
+import sys
+sys.stdin = open('./input.txt', 'r')
+input = sys.stdin.readline
+
+N = int(input())
+
+if N % 2 == 0:
+    print('CY')
+else:
+    print('SK')
+
