a3ac44268c399556909a263b3413defae86ebae8
hyeongrok <hhr@hhr.kr>
2022-03-15 20:36:46 +0900

    220315

--- test.py
+++ test.py
@@ -1,36 +1,29 @@
+"""
+problem tier : Silver 3 (solved.ac)
+"""
+
+import sys
+# sys.stdin = open('./input.txt', 'r')
+input = sys.stdin.readline
 
 
-N = int(input())
-A = list(map(int, input().split()))
+stack = []
 
-# print(N, A)
-
-
-permutation = []
-ancient = [False for i in range(len(A))]
-max_permutation = 0
-def BT():
-    # print(permutation, ancient)
-    global max_permutation
-    if len(permutation) == N:
-        result_permutation = 0
-        for i in range(N-1):
-            result_permutation += abs(permutation[i] - permutation[i+1])
-        if max_permutation < result_permutation:
-            max_permutation = result_permutation
+def bt():
+    if len(stack) == N:
+        print(' '.join(map(str, stack)))
         return
-    for i in range(len(A)):
-        if not ancient[i]:
-            ancient[i] = True
-            permutation.append(A[i])
-            BT()
-            permutation.pop()
-            ancient[i] = False
-BT()
-print(max_permutation)
+    for i in range(1, N+1):
+        if i not in stack:
+            stack.append(i)
+            bt()
+            stack.pop()
 
 
+def solution():
+    global N
+    N = int(input())
+    bt()
 
 
-
-
+solution()
